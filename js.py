2e531e5d77c27cfa9103ef8516882135ff380bfe
Daniil-Potebnia <106385707+Daniil-Potebnia@users.noreply.github.com>
2023-12-14 18:36:15 +0300

    Update js.py

--- js.py
+++ js.py
@@ -11,6 +11,9 @@ class Person:
         with open('persons.json', mode='w') as f:
             f.write(json.dumps({'name': self.name, 'age': self.age, 'computer': self.computer.get_json()}))
 
+    def __str__(self):
+        return f'{self.name} {self.age}'
+
 
 class Computer:
     def __init__(self, tip):
@@ -22,3 +25,14 @@ class Computer:
 
     def get_json(self):
         return {'tip': self.tip}
+
+
+def create_from_json(struct):
+    if struct == 'person':
+        with open('persons.json', mode='r') as f:
+            text = json.loads(f.read())
+            return Person(text['name'], text['age'], Computer(text['computer']['tip']))
+    elif struct == 'computer':
+        with open('computers.json', mode='r') as f:
+            text = json.loads(f.read())
+            return Computer(text['computer']['tip'])
